c12dbc0476815657cfe140ce73b0ce5e4395834a
Luis Martinez <LuisMartinez@Luiss-MacBook-Pro.local>
2018-01-21 03:53:06 -0800

    trying to slide to thru chapter pages and stop the back button after Resources page

--- Study-Hack2/viewcontroller2.swift
+++ Study-Hack2/viewcontroller2.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  viewcontroller2.swift
 //  Study-Hack2
 //
 //  Created by Luis Martinez on 1/21/18.
@@ -8,17 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class viewcontroller2: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.setNavigationBarHidden(true, animated: false)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 }
 
